21ec872d45631404633c852849375da1b1586726
Volkan <volkankalin07@gmail.com>
2022-06-21 16:35:46 +0300

    fix uri (mixed content)

--- examples/Client.UI/src/utils/axiosInstance.ts
+++ examples/Client.UI/src/utils/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   // baseURL: "https://actimi-strapi.herokuapp.com/partner",
   // baseURL: "https://localhost:7002",
-  baseURL: "http://api.actimi.health/partner",
+  baseURL: "https://api.actimi.health/partner",
   withCredentials: true,
 });
 
